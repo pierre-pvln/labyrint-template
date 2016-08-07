e58c2f772e398fa89cc1d1f8cf18c9bc5f00d2d3
Pierre Veelen <pierre.veelen@pvln.nl>
2016-08-07 17:39:45 +0200

    Eerste aanpassingen in template

---
+++ set_version.cmd
@@ -0,0 +1,7 @@
+:: Name:     set_version.cmd
+:: Purpose:  set the version as an environment variable
+:: Author:   pierre.veelen@pvln.nl
+:: Revision: 2016 08 01 - initial version
+
+:: -VERSION
+SET version=v3.2.1
